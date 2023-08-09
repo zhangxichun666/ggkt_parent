628c58e6b5c6770dae9a6f75b343fb1cdd8770ff
ZhangWenFei <1035605677@qq.com>
2023-08-09 22:48:11 +0800

    张文飞 第一次提交 完成讲师模块，查询所有讲师和根据ID删除讲师

--- service/service_vod/src/main/java/com/atguigu/ggkt/vod/ServiceVodApplication.java
+++ service/service_vod/src/main/java/com/atguigu/ggkt/vod/ServiceVodApplication.java
@@ -2,8 +2,10 @@ package com.atguigu.ggkt.vod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.atguigu")
 public class ServiceVodApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceVodApplication.class, args);
--- service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
+++ service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
@@ -1,11 +1,15 @@
 package com.atguigu.ggkt.vod.controller;
 
 
+import com.atguigu.ggkt.model.vod.Teacher;
 import com.atguigu.ggkt.vod.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -15,11 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zxc
  * @since 2023-08-09
  */
+
+@Api(tags = "讲师管理模块")
 @RestController
 @RequestMapping("/admin/vod/teacher")
 public class TeacherController {
     @Autowired
     private TeacherService teacherService;
 
+    //查询所有讲师
+    @ApiOperation("查询所有讲师")
+    @GetMapping("findAll")
+    public List<Teacher> findAllTeacher(){
+        List<Teacher> list = teacherService.list();
+        return list;
+    }
+    //删除讲师
+    @ApiOperation("根据ID逻辑删除讲师")
+    @DeleteMapping("remove/{id}")
+    public boolean removeTeacher(@PathVariable Long id){
+        boolean isSuccess = teacherService.removeById(id);
+        return isSuccess;
+    }
 }
 
